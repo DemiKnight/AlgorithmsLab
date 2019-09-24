abf0a7ee6d5a7876d195adf062cd85d4becd9c55
Kryptical <alexknight133@icloud.com>
2019-09-24 10:39:22 +0100

    Init files
    1) Completed Exercise 1

---
+++ Test/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOne, numberTwo;
+//            string consoleInput;
+            /*
+            Console.Write("Enter First Number: ");
+            numberOne = Convert.ToInt16(Console.ReadLine());
+            
+            Console.Write("Enter Second Number: ");
+            numberTwo = Convert.ToInt16(Console.ReadLine());
+
+            Console.WriteLine("Sum: {0}", sum(ref numberOne, ref numberTwo));
+            Console.WriteLine("Power: {0}", power(ref numberOne, ref numberTwo));
+            */
+
+            inputAverages();
+        }
+
+        static int sum(ref int xNew, ref int yNew)
+        {
+            return xNew + yNew;
+        }
+
+        static int power(ref int baseNum, ref int indexNumber)
+        {
+            int returnNumber = baseNum;
+            
+            for (int index = 1; index < indexNumber; index++)
+                returnNumber *= baseNum;
+            
+            return returnNumber;
+        }
+
+        static void inputAverages()
+        {
+            int totalInputVal = 0, index = 0;
+            bool loop = true;
+            
+            Console.WriteLine("Enter \'0\' to escape and calculate the average.");
+            while (loop)
+            {
+                Console.WriteLine("Enter Number: ");
+                int tempInput = Convert.ToInt16(Console.ReadLine());
+                
+                
+                if (totalInputVal + tempInput == totalInputVal)
+                {
+                    loop = false;
+                }
+                else
+                {
+                    totalInputVal += tempInput;
+                    index++;
+                }
+            }
+            
+            Console.WriteLine("Average: {0}", totalInputVal/index);
+        }
+    }
+}
