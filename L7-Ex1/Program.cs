c0468c6719767a5c5e19dd982eeb877b8901b9fb
Alex Knight <18016816@stu.mmu.ac.uk>
2019-11-12 10:48:28 +0000

    Added Recursion lab ex 1

---
+++ L7-Ex1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace L7_Ex1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            printNumbers(5);
+            Console.ReadLine();
+        }
+        
+        static void printNumbers(int n)
+        {
+            if (n>0)
+            {
+                Console.WriteLine(" n = {0} ", n);
+                printNumbers ( n - 1 );
+            }
+        }
+    }
+}
